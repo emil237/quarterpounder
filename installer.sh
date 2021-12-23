0a61544978af847eab03e7ca36008a7609f28df2
emil237 <95539537+emil237@users.noreply.github.com>
2021-12-23 18:26:41 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,37 +1,44 @@
 #!/bin/sh
 #
-# command wget
-https://raw.githubusercontent.com/emil237/quarterpounder/main/installer.sh -O - | /bin/sh
+#command wget https://raw.githubusercontent.com/emil237/quarterpounder/main/installer.sh -O - | /bin/sh  
 #
 #
-echo ' ============================================================================'
-echo " plugin quarterpouder "
-
-my_main_url="https://raw.githubusercontent.com/emil237/"
-my_ipk="quarterpounder.ipk"
-package_dir='quarterpounder/main/'
-MY_URL=$my_main_url$package_dir'/'$my_ipk
-
-MY_TMP_FILE="/tmp/my_ipk"
-echo '**********************************************************'
-echo " download plugin "
-
-wget -T 2 $MY_URL -P "/tmp/"
-echo " install plugin "
-echo ""
-echo ""
-echo ''
-	if which dpkg > /dev/null 2>&1; then
-		apt-get install --reinstall $MY_TMP_FILE -y
-	else
-		opkg install --force-reinstall $MY_TMP_FILE
-	fi
-echo " *************************************************************************************************************************"
-echo "     WELCOME TO PLUGIN QUARTERPOUDER "
-echo "             UPLOADED BY EMIL_NABIL "
+#
+echo ' download and install plugin quarterpouder '
+echo ' ====================================================================='
+wget -O /var/volatile/tmp/QuarterPounder.tar.gz "https://raw.githubusercontent.com/emil237/quarterpounder/main/QuarterPounder.tar.gz"
+wait 
+tar xzvpf /tmp/*.tar.gz  -C /
 wait
-sleep 2;
+rm -r /var/volatile/tmp/QuarterPounder.tar.gz                         
+
+echo "========================================================================"
+                
+	echo ".        WELCOME TO QUARTERPOUNDER "
+	
+echo "#    SCRIPT INSTALLED SUCCESSFULLY           #"
+echo "        UPLOADED BY EMIL_NABIL  "  
+sleep 4;   
+echo "#########################################################"
+echo "#########################################################"
+wait
+sleep 2
 exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
