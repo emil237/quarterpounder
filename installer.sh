1f88de6149cd0aba91b05f6d4b42b6c7ba0fb9d6
emil237 <95539537+emil237@users.noreply.github.com>
2021-12-23 19:13:38 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ echo " plugin quarterpouder "
 my_main_url="https://raw.githubusercontent.com/emil237/"
 my_ipk="quarterpounder-latest.ipk"
 package_dir='quarterpounder/main/'
-MY_URL=$my_main_url$package_dir'/'$my_ipk
+MY_URL="$my_main_url$package_dir'/'$my_ipk"
 
 MY_TMP_FILE="/tmp/my_ipk"
 echo '**********************************************************'
@@ -32,6 +32,7 @@ echo "             UPLOADED BY EMIL_NABIL "
 wait
 sleep 2;
 exit 0
+
 
 
 
